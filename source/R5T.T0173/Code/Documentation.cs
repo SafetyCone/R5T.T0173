6c696818359fe6293683e2b3d215e00ab088b9ea
davidcoats <david@getsafetycone.com>
2023-03-09 23:44:54 -0800

    Initial commit.

---
+++ source/R5T.T0173/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0173
+{
+	/// <summary>
+	/// Strongly-typed description XML types library.
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
---
+++ source/R5T.T0173/Project Plan.md
@@ -0,0 +1,2 @@
+# R5T.T0173
+Strongly-typed description XML types library.
